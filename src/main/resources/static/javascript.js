ffd327810676e43311b23b0c54fa30d8e40f896a
KilledByACake <elisabeth0705@live.no>
2024-04-25 21:26:19 +0200

    DRITTPROGRAM

--- src/main/resources/static/javascript.js
+++ src/main/resources/static/javascript.js
@@ -1,8 +1,7 @@
-function bestillBilletter () {
-    let feil = 0; // Teller for å spore antall feil i valideringen
-    // Henter verdier og validerer dem
+function bestillBilletter() {
+    let feil = 0;
     let bestilling = {
-        film: $("#velgFilm").val(),
+        film: $("#film").val(),
         antall: $("#antall").val(),
         fornavn: $("#fornavn").val(),
         etternavn: $("#etternavn").val(),
@@ -10,85 +9,58 @@ function bestillBilletter () {
         telefonnr: $("#telefonnr").val(),
         epost: $("#epost").val()
     };
+
     // Validering av filmvalg
-    if (bestilling.film === "") {
-        $("#filmError").html("<span style='color: deeppink'>Vennligst velg en film</span>");
-        feil++;
-    } else {
-        $("#filmError").empty();
-    }
+    $("#filmError").html(bestilling.film === "" ? "<span style='color: deeppink'>Vennligst velg en film</span>" : "");
+    feil += bestilling.film === "" ? 1 : 0;
 
-// Validering av antall billetter
-    if (isNaN(bestilling.antall) || bestilling.antall === "") {
-        $("#antallError").html("<span style='color: deeppink'>Vennligst angi antall billetter</span>");
-        feil++;
-    } else {
-        $("#antallError").empty();
-    }
+    // Validering av antall billetter
+    $("#antallError").html((isNaN(bestilling.antall) || bestilling.antall === "") ? "<span style='color: deeppink'>Vennligst angi antall billetter</span>" : "");
+    feil += (isNaN(bestilling.antall) || bestilling.antall === "") ? 1 : 0;
 
-// Validering av fornavn
-    if (bestilling.fornavn === "" || !isNaN(bestilling.fornavn)) {
-        $("#fornavnError").html("<span style='color: deeppink'>Vennligst skriv inn ditt fornavn</span>");
-        feil++;
-    } else {
-        $("#fornavnError").empty();
-    }
+    // Validering av fornavn
+    $("#fornavnError").html((bestilling.fornavn === "" || !isNaN(bestilling.fornavn)) ? "<span style='color: deeppink'>Vennligst skriv inn ditt fornavn</span>" : "");
+    feil += (bestilling.fornavn === "" || !isNaN(bestilling.fornavn)) ? 1 : 0;
 
-// Validering av etternavn
-    if (bestilling.etternavn === "" || !isNaN(bestilling.etternavn)) {
-        $("#etternavnError").html("<span style='color: deeppink'>Vennligst skriv inn ditt etternavn</span>");
-        feil++;
-    } else {
-        $("#etternavnError").empty();
-    }
+    // Validering av etternavn
+    $("#etternavnError").html((bestilling.etternavn === "" || !isNaN(bestilling.etternavn)) ? "<span style='color: deeppink'>Vennligst skriv inn ditt etternavn</span>" : "");
+    feil += (bestilling.etternavn === "" || !isNaN(bestilling.etternavn)) ? 1 : 0;
 
-// Validering av adresse
-    if (bestilling.adresse === "") {
-        $("#adresseError").html("<span style='color: deeppink'>Vennligst skriv inn din adresse</span>");
-        feil++;
-    } else {
-        $("#adresseError").empty();
-    }
+    // Validering av adresse
+    $("#adresseError").html(bestilling.adresse === "" ? "<span style='color: deeppink'>Vennligst skriv inn din adresse</span>" : "");
+    feil += bestilling.adresse === "" ? 1 : 0;
 
-// Validering av telefonnummer
-    if (!bestilling.telefonnr.match(/^[0-9]{8}$/)) {
-        $("#telefonnrError").html("<span style='color: deeppink'>Vennligst skriv inn et gyldig telefonnummer</span>");
-        feil++;
-    } else {
-        $("#telefonnrError").empty();
-    }
+    // Validering av telefonnummer
+    $("#telefonnrError").html(!bestilling.telefonnr.match(/^[0-9]{8}$/) ? "<span style='color: deeppink'>Vennligst skriv inn et gyldig telefonnummer</span>" : "");
+    feil += !bestilling.telefonnr.match(/^[0-9]{8}$/) ? 1 : 0;
 
-// Validering av epost
-    if (!bestilling.epost.match(/^[A-Za-z\._\-0-9]+@[A-Za-z]+[\.][a-z]{2,4}$/)) {
-        $("#epostError").html("<span style='color: deeppink'>Vennligst skriv inn en gyldig epostadresse</span>");
-        feil++;
-    } else {
-        $("#epostError").empty();
-    }
+    // Validering av epost
+    $("#epostError").html(!bestilling.epost.match(/^[A-Za-z\._\-0-9]+@[A-Za-z]+[\.][a-z]{2,4}$/) ? "<span style='color: deeppink'>Vennligst skriv inn en gyldig epostadresse</span>" : "");
+    feil += !bestilling.epost.match(/^[A-Za-z\._\-0-9]+@[A-Za-z]+[\.][a-z]{2,4}$/) ? 1 : 0;
 
-    // Sjekker om alle feltene er korrekt utfylt og sender bestillingen
     if (feil === 0) {
-        $.post("/lagreBestillinger", bestilling, function () {henteBestilling();});
+        $.post("/lagreBestillinger", bestilling, function () {
+            henteBestillinger();
+            $("#film").prop("selectedIndex", 0);
+            $("#antall").val("");
+            $("#fornavn").val("");
+            $("#etternavn").val("");
+            $("#adresse").val("");
+            $("#telefonnr").val("");
+            $("#epost").val("");
+        });
     } else {
         alert("Vennligst fyll ut alle feltene korrekt.");
     }
 
-    // Tømmer feltene
-    $("#velgFilm option").eq(0).prop("selected", true);
-    $("#antall").val(""),
-    $("#fornavn").val(""),
-    $("#etternavn").val(""),
-    $("#adresse").val(""),
-    $("#telefonnr").val(""),
-    $("#epost").val("")
 
     //Hente bestillinger
     function henteBestillinger() {
-        $.post("/henteBestillinger", bestilling, function () {henteBestillinger();});
+        $.post("/henteBestillinger", bestilling, function () {formaterData();});
     }
 
     //
-    function formaterData(bestilling) {
+    function formaterData(bestillinger) {
         // Fjerner billettene som er printet ut(bare de som vises på siden ikke de på db) og deretter printer ny sortert liste
         $("#henteBestillinger tr").remove();
         bestillinger.forEach(function(bestilling) {
